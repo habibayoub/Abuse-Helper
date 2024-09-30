98abf623daa698413fc05c2335561fef17b2771d
hate <15314665+hate@users.noreply.github.com>
2024-09-30 14:02:32 -0400

    remove keycloak from backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,8 +5,6 @@ mod postgres;
 mod routes;
 
 use actix_web::{get, web, App, HttpResponse, HttpServer};
-// use std::{thread, time::Duration};
-// use actix_web_middleware_keycloak_auth::{AlwaysReturnPolicy, DecodingKey, KeycloakAuth, Role};
 use dotenv::dotenv;
 use routes::{customer, email, nctns, util};
 
@@ -21,7 +19,7 @@ async fn api_status() -> HttpResponse {
 async fn main() -> std::io::Result<()> {
     // Load start-up environment variables and set the log level
     dotenv().ok();
-    std::env::set_var("RUST_LOG", "info,actix_web_middleware_keycloak_auth=trace");
+    std::env::set_var("RUST_LOG", "info");
     env_logger::init();
 
     // Create the database pool and run migrations
@@ -40,35 +38,16 @@ async fn main() -> std::io::Result<()> {
     let address = std::env::var("ADDRESS").unwrap_or_else(|_| "127.0.0.1:8000".into());
 
     HttpServer::new(move || {
-        // Create the Keycloak authentication middleware
-        // let keycloak_public_key =
-        //     std::env::var("KEYCLOAK_PUBLIC_KEY").expect("KEYCLOAK_PUBLIC_KEY must be set.");
-
-        // let keycloak_auth = KeycloakAuth {
-        //     detailed_responses: true,
-        //     passthrough_policy: AlwaysReturnPolicy,
-        //     keycloak_oid_public_key: DecodingKey::from_rsa_pem(keycloak_public_key.as_bytes())
-        //         .unwrap(),
-        //     required_roles: vec![Role::Realm {
-        //         role: "admin".to_owned(),
-        //     }],
-        // };
-
         // Create the Actix application
         App::new()
             .app_data(web::Data::new(pg_pool.clone())) // Add the database pool to the application
             .service(api_status)
             .service(
                 web::scope("/customer")
-                    // .wrap(keycloak_auth) // Enable Keycloak authentication
                     .service(customer::list)
                     .service(customer::find),
             )
-            .service(
-                web::scope("/nctns")
-                    // .wrap(keycloak_auth)
-                    .service(nctns::list),
-            )
+            .service(web::scope("/nctns").service(nctns::list))
             .service(web::scope("/util").service(util::whois))
             .service(web::scope("/email").service(email::send))
     })
